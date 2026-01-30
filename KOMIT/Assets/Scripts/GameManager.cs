037cbba88df2895916d1d11f15599d46630c6623
Darwin-our-lord <David@prebensen.dk>
2026-01-30 02:13:49 +0100

    actual acceptable version

--- KOMIT/Assets/Scripts/GameManager.cs
+++ KOMIT/Assets/Scripts/GameManager.cs
@@ -3,9 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using TMPro;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,11 +10,7 @@ public enum miniGames
 {
     Keypads,
     ColorPick
-
-
-
 }
-
 
 public class GameManager : AttributesSync
 {
@@ -25,20 +18,13 @@ public class GameManager : AttributesSync
     public GameObject alterunaMenu;
 
     [Header("Game Data")]
-
-    [SynchronizableField]
-    public int currentMinigame = 0;
-
-    [SynchronizableField]
-    public static int minigamesCompleted = 0;
+    [SynchronizableField] public int currentMinigame = 0;
+    [SynchronizableField] public static int minigamesCompleted = 0;
 
     public bool minigameRunning = false;
 
-    [SynchronizableField]
-    private bool updateMiniGameRunning = false;
-
-    [SynchronizableField]
-    private bool minigameChosen = false;
+    [SynchronizableField] private bool triggerReset = false;
+    [SynchronizableField] private bool minigameChosen = false;
 
     static System.Random _R = new System.Random();
     static miniGames RandomEnumValue<miniGames>()
@@ -49,17 +35,19 @@ public class GameManager : AttributesSync
 
     private void Update()
     {
-        if ( Multiplayer.Me.Index == 1 && updateMiniGameRunning)
+        if (triggerReset)
         {
-            updateMiniGameRunning = false;
-            minigameRunning = false;
-            minigameChosen = false;
-            player1UI.SetActive(false);
-            player2UI.SetActive(false);
-            player1UI_CP.SetActive(false);
-            player2UI_CP.SetActive(false);
+            ResetAllMinigames();
+            if (Multiplayer.Me.Index == 0)
+            {
+                triggerReset = false;
+                Commit();
+            }
+            return;
         }
+
         if (minigameRunning) return;
+
         if (Multiplayer.GetUsers().Count == 2)
         {
             if (Multiplayer.Me.Index == 0)
@@ -70,16 +58,8 @@ public class GameManager : AttributesSync
                     miniGames minigame = RandomEnumValue<miniGames>();
                     switch (minigame)
                     {
-
-                        case miniGames.Keypads:
-                            currentMinigame = 2;
-                            break;
-                        case miniGames.ColorPick:
-                            currentMinigame = 3;
-                            break;
-                        default:
-                            Debug.LogError("no minigame was selected - you're a dumbass, dumbass");
-                            return;
+                        case miniGames.Keypads: currentMinigame = 2; break;
+                        case miniGames.ColorPick: currentMinigame = 3; break;
                     }
                     minigameChosen = true;
                     Commit();
@@ -87,37 +67,14 @@ public class GameManager : AttributesSync
             }
             else
             {
-                if (currentMinigame == 0)
-                {
-                    Debug.LogWarning("waiting for minigame sync...");
-                    return;
-                }
-
+                if (currentMinigame == 0) return;
             }
-            Debug.LogError("FUCK YOU  -  "+currentMinigame);
 
             switch (currentMinigame)
             {
-                case 2:
-                    Debug.LogError("done!!!");
-                    minigameRunning = true;
-                    Commit();
-                    PlayKeypads();
-                   
-                    break;
-                case 3:
-                    Debug.LogError("done!!!");
-                    minigameRunning = true;
-                    Commit();
-                    PlayColorPickerGame();
-                    break;
-
-                case 0:
-                    Debug.LogError("no minigame was selected - you're a dumbass, dumbass\n ohh and its in like the second thing btw<3");
-                    break;
-
+                case 2: minigameRunning = true; PlayKeypads(); break;
+                case 3: minigameRunning = true; PlayColorPickerGame(); break;
             }
-
 
             if (alterunaMenu.activeSelf) alterunaMenu.SetActive(false);
         }
@@ -125,41 +82,68 @@ public class GameManager : AttributesSync
         {
             if (!alterunaMenu.activeSelf) alterunaMenu.SetActive(true);
         }
+    }
 
+    private void ResetAllMinigames()
+    {
+        minigameRunning = false;
+        minigameChosen = false;
+        currentMinigame = 0;
+        needsWait = true;
 
+        player1UI.SetActive(false);
+        player2UI.SetActive(false);
+        player1UI_CP.SetActive(false);
+        player2UI_CP.SetActive(false);
+
+        answerSpritesID.Clear();
+        orderSpritesID.Clear();
+        answerSpritesID_CP = new List<int>();
+        answerSpritesIDColor_CP = new List<int>();
+
+        currentStageIndex = 0;
+        lossesAmount = 0;
+        completedAmount_CP = 0;
+        lossesAmount_CP = 0;
+
+        // Reset Keypad Buttons
+        foreach (var obj in player1SpritesOBJ)
+        {
+            if (obj != null) obj.GetComponent<Image>().color = Color.white;
+        }
+
+        // Reset Color Picker Drag Objects
+        foreach (var obj in player1SpritesOBJ_CP)
+        {
+            if (obj != null)
+            {
+                UIDrag dragScript = obj.GetComponent<UIDrag>();
+                if (dragScript != null)
+                {
+                    dragScript.ResetDragObject();
+                }
+            }
+        }
+
+        foreach (var obj in player1ColorOBJ_CP)
+        {
+            if (obj != null) obj.GetComponent<Image>().color = Color.white;
+        }
     }
 
     #region keypadsGame
-
     [Header("--KeypadsGame--")]
-
     public List<Sprite> allSprites = new List<Sprite>();
-
-
     public GameObject player1UI;
-
     public GameObject[] player1SpritesOBJ;
-
-
     public GameObject player2UI;
-
     public GameObject[] player2SpritesOBJ;
 
-    [SynchronizableField]
-    bool needsWait = true;
-
-    [SynchronizableField]
-    private List<int> answerSpritesID = new List<int>();
-
-    [SynchronizableField]
-    private List<int> orderSpritesID = new List<int>();
-
-
-    [SynchronizableField]
-    private int orderSpriteAmount = 6;
-
-    [SynchronizableField]
-    private int answerSpriteAmount = 4;
+    [SynchronizableField] bool needsWait = true;
+    [SynchronizableField] private List<int> answerSpritesID = new List<int>();
+    [SynchronizableField] private List<int> orderSpritesID = new List<int>();
+    [SynchronizableField] private int orderSpriteAmount = 6;
+    [SynchronizableField] private int answerSpriteAmount = 4;
 
     private int currentStageIndex = 0;
     private int lossesAmount = 0;
@@ -170,19 +154,15 @@ public class GameManager : AttributesSync
         {
             needsWait = true;
             currentStageIndex = 0;
-            answerSpritesID.Clear();
-
-
-
-            while (answerSpritesID.Count < answerSpriteAmount)
+            List<int> tempAns = new List<int>();
+            while (tempAns.Count < answerSpriteAmount)
             {
                 int s = UnityEngine.Random.Range(0, allSprites.Count);
-                if (!answerSpritesID.Contains(s)) answerSpritesID.Add(s);
+                if (!tempAns.Contains(s)) tempAns.Add(s);
             }
+            answerSpritesID = tempAns;
 
-            orderSpritesID = new List<int>(new int[orderSpriteAmount]);
-
-            //makes a list of 6 numbers, randomly shuffles them, picks the first 4, puts them in order from smallest to largest
+            List<int> tempOrder = new List<int>(new int[orderSpriteAmount]);
             var slots = Enumerable.Range(0, orderSpriteAmount).OrderBy(_ => UnityEngine.Random.value).Take(answerSpriteAmount).OrderBy(x => x).ToList();
 
             int ansIndex = 0;
@@ -190,52 +170,41 @@ public class GameManager : AttributesSync
             {
                 if (slots.Contains(i))
                 {
-                    orderSpritesID[i] = answerSpritesID[ansIndex];
+                    tempOrder[i] = answerSpritesID[ansIndex];
                     ansIndex++;
                 }
                 else
                 {
-                    do { orderSpritesID[i] = UnityEngine.Random.Range(0, allSprites.Count); }
-                    while (answerSpritesID.Contains(orderSpritesID[i]) || orderSpritesID.IndexOf(orderSpritesID[i]) != i);
+                    int randS;
+                    do { randS = UnityEngine.Random.Range(0, allSprites.Count); }
+                    while (answerSpritesID.Contains(randS) || tempOrder.Contains(randS));
+                    tempOrder[i] = randS;
                 }
             }
+            orderSpritesID = tempOrder;
             needsWait = false;
             Commit();
-
         }
-        Debug.LogWarning("waiting2---");
-        if (needsWait) { StartCoroutine(WaitThenRestart()); return; }
-        Debug.Log("done2");
 
-        //answer guy-------------------------------------------------------------------------------------------------
+        if (needsWait || answerSpritesID.Count < answerSpriteAmount) { StartCoroutine(WaitThenRestart()); return; }
+
         if (Multiplayer.Instance.Me.Index == 0)
         {
             player1UI.SetActive(true);
-
             for (int i = 0; i < answerSpriteAmount; i++)
             {
                 player1SpritesOBJ[i].GetComponent<Image>().sprite = allSprites[answerSpritesID[i]];
                 player1SpritesOBJ[i].GetComponent<KeypadButton>().SetIndex(i);
             }
-
         }
-
-
-
-
-        //order guy--------------------------------------------------------------------------------------------------
         else if (Multiplayer.Instance.Me.Index == 1)
         {
             player2UI.SetActive(true);
-
             for (int i = 0; i < orderSpriteAmount; i++)
             {
                 player2SpritesOBJ[i].GetComponent<Image>().sprite = allSprites[orderSpritesID[i]];
             }
-
         }
-
-
     }
 
     public void keyPadMinigameButton(int index)
@@ -244,18 +213,14 @@ public class GameManager : AttributesSync
         {
             currentStageIndex++;
             player1SpritesOBJ[index].GetComponent<Image>().color = Color.green;
-
             if (currentStageIndex >= answerSpriteAmount)
             {
-                minigamesCompleted++;
-                Debug.LogError("GAME WON______");
-                minigameRunning = false;
-                updateMiniGameRunning = true;
-                minigameChosen = false;
-                currentMinigame = 0;
-                player1UI.SetActive(false);
-                player2UI.SetActive(false);
-                Commit();
+                if (Multiplayer.Me.Index == 0)
+                {
+                    minigamesCompleted++;
+                    triggerReset = true;
+                    Commit();
+                }
             }
         }
         else
@@ -264,50 +229,26 @@ public class GameManager : AttributesSync
             if (lossesAmount < 3)
             {
                 currentStageIndex = 0;
-                for (int i = 0; i < player1SpritesOBJ.Length; i++)
-                {
-                    player1SpritesOBJ[i].GetComponent<Image>().color = Color.white;
-                }
-            }
-            else
-            {
-                Debug.LogError("GAME LOST!!!");
+                foreach (var obj in player1SpritesOBJ) obj.GetComponent<Image>().color = Color.white;
             }
         }
     }
 
-    public IEnumerator WaitThenRestart()
-    {
-        yield return new WaitForSeconds(0.1f);
-        PlayKeypads();
-    }
-
+    public IEnumerator WaitThenRestart() { yield return new WaitForSeconds(0.1f); PlayKeypads(); }
     #endregion
 
-
     #region colorpickergame
-
     [Header("--ColorPickerGame--")]
-
     public List<Color> allColors = new List<Color>();
-
     public GameObject player1UI_CP;
-
     public GameObject[] player1SpritesOBJ_CP;
     public GameObject[] player1ColorOBJ_CP;
-
     public GameObject player2UI_CP;
-
     public GameObject[] player2SpritesOBJ_CP;
 
-    [SynchronizableField]
-    public List<int> answerSpritesID_CP = new List<int>();
-
-    [SynchronizableField]
-    public List<int> answerSpritesIDColor_CP = new List<int>();
-
-    [SynchronizableField]
-    private int answerSpriteAmount_CP = 4;
+    [SynchronizableField] public List<int> answerSpritesID_CP = new List<int>();
+    [SynchronizableField] public List<int> answerSpritesIDColor_CP = new List<int>();
+    [SynchronizableField] private int answerSpriteAmount_CP = 4;
 
     private int completedAmount_CP = 0;
     private int lossesAmount_CP = 0;
@@ -317,37 +258,27 @@ public class GameManager : AttributesSync
         if (Multiplayer.Me.Index == 0)
         {
             needsWait = true;
-            Commit();
-
             completedAmount_CP = 0;
+            List<int> tempS = new List<int>();
+            List<int> tempC = new List<int>();
 
-            // CHANGE: Create local temporary lists. 
-            // Assigning a new list reference is more reliable for sync than clearing/adding.
-            List<int> tempSprites = new List<int>();
-            List<int> tempColors = new List<int>();
-
-            while (tempSprites.Count < answerSpriteAmount_CP)
+            while (tempS.Count < answerSpriteAmount_CP)
             {
                 int s = UnityEngine.Random.Range(0, allSprites.Count);
-                if (!tempSprites.Contains(s)) tempSprites.Add(s);
+                if (!tempS.Contains(s)) tempS.Add(s);
             }
-
-            while (tempColors.Count < answerSpriteAmount_CP)
+            while (tempC.Count < answerSpriteAmount_CP)
             {
-                int s = UnityEngine.Random.Range(0, allColors.Count);
-                if (!tempColors.Contains(s)) tempColors.Add(s);
+                int c = UnityEngine.Random.Range(0, allColors.Count);
+                if (!tempC.Contains(c)) tempC.Add(c);
             }
 
-            // CHANGE: Assign the full lists to the synced variables at once
-            answerSpritesID_CP = tempSprites;
-            answerSpritesIDColor_CP = tempColors;
-
+            answerSpritesID_CP = tempS;
+            answerSpritesIDColor_CP = tempC;
             needsWait = false;
             Commit();
         }
 
-        // CHANGE: Improved safety check. 
-        // We wait if the host says so OR if the data hasn't actually arrived yet.
         if (needsWait || answerSpritesID_CP.Count < answerSpriteAmount_CP)
         {
             StartCoroutine(WaitThenRestartColorPicker());
@@ -357,23 +288,17 @@ public class GameManager : AttributesSync
         if (Multiplayer.Instance.Me.Index == 0)
         {
             player1UI_CP.SetActive(true);
-
-            for (int i = 0; i < answerSpriteAmount_CP; i++) // CHANGE: Fixed variable name to answerSpriteAmount_CP
+            for (int i = 0; i < answerSpriteAmount_CP; i++)
             {
                 player1SpritesOBJ_CP[i].GetComponent<Image>().sprite = allSprites[answerSpritesID_CP[i]];
                 player1ColorOBJ_CP[i].GetComponent<Image>().color = allColors[answerSpritesIDColor_CP[i]];
-
                 player1ColorOBJ_CP[i].GetComponent<ColorZone>().SetAnswerId(i);
                 player1SpritesOBJ_CP[i].GetComponent<UIDrag>().SetAnswerId(i);
             }
-            // CHANGE: Removed Commit() from inside the loop (it slows down performance)
         }
-
-        if (Multiplayer.Instance.Me.Index == 1)
+        else if (Multiplayer.Instance.Me.Index == 1)
         {
             player2UI_CP.SetActive(true);
-
-            // CHANGE: Create NEW copies so we don't accidentally sync the "Extra" items back to the host
             List<int> p2Sprites = new List<int>(answerSpritesID_CP);
             List<int> p2Colors = new List<int>(answerSpritesIDColor_CP);
 
@@ -382,16 +307,14 @@ public class GameManager : AttributesSync
                 int s = UnityEngine.Random.Range(0, allSprites.Count);
                 if (!p2Sprites.Contains(s)) p2Sprites.Add(s);
             }
-
             while (p2Colors.Count < answerSpriteAmount_CP + 1)
             {
-                int s = UnityEngine.Random.Range(0, allColors.Count);
-                if (!p2Colors.Contains(s)) p2Colors.Add(s);
+                int c = UnityEngine.Random.Range(0, allColors.Count);
+                if (!p2Colors.Contains(c)) p2Colors.Add(c);
             }
 
             for (int i = 0; i < p2Sprites.Count; i++)
             {
-                // CHANGE: Use the local "p2" lists and added an array length safety check
                 if (i < player2SpritesOBJ_CP.Length)
                 {
                     player2SpritesOBJ_CP[i].GetComponent<Image>().sprite = allSprites[p2Sprites[i]];
@@ -400,46 +323,25 @@ public class GameManager : AttributesSync
             }
         }
     }
+
     public void CheckRightColorMatch(int objNR, int colorNR)
     {
-        Debug.LogError("checking...");
         if (objNR == colorNR)
         {
             completedAmount_CP++;
-            Debug.LogError("win");
-            if (completedAmount_CP == answerSpriteAmount)
+            if (completedAmount_CP >= answerSpriteAmount_CP)
             {
-                minigamesCompleted++;
-                Debug.LogError("GAME WON______");
-                minigameRunning = false;
-                updateMiniGameRunning = true;
-                minigameChosen = false;
-                currentMinigame = 0;
-                player1UI_CP.SetActive(false);
-                player2UI_CP.SetActive(false);
-                Commit();
+                if (Multiplayer.Me.Index == 0)
+                {
+                    minigamesCompleted++;
+                    triggerReset = true;
+                    Commit();
+                }
             }
-
-
         }
-        else
-        {
-            lossesAmount_CP++;
-            Debug.LogError("loss");
-            if (lossesAmount_CP == 3)
-            {
-
-            }
-
-        }
-    }
-    public IEnumerator WaitThenRestartColorPicker()
-    {
-        Debug.LogError("waiting..");
-        yield return new WaitForSeconds(0.1f);
-        PlayColorPickerGame();
+        else { lossesAmount_CP++; }
     }
 
+    public IEnumerator WaitThenRestartColorPicker() { yield return new WaitForSeconds(0.1f); PlayColorPickerGame(); }
     #endregion
-
 }
--- KOMIT/Assets/Scripts/UIDrag.cs
+++ KOMIT/Assets/Scripts/UIDrag.cs
@@ -1,19 +1,36 @@
 using Alteruna;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
     private Canvas canvas;
+    private Vector2 originalPosition;
+    private Image image;
+    private bool isInitialized = false; // Track if we have references
 
     bool checkforCollision = false;
-    [SerializeField] private int answerId; // ID of this answer
+    [SerializeField] private int answerId;
     public GameManager manager;
+
     void Awake()
     {
+        Initialize();
+    }
+
+    // Moved initialization to a separate method so it can be called safely
+    private void Initialize()
+    {
+        if (isInitialized) return;
+
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
+        image = GetComponent<Image>();
+        originalPosition = rectTransform.anchoredPosition;
+
+        isInitialized = true;
     }
 
     public void SetAnswerId(int id)
@@ -21,8 +38,21 @@ public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
         answerId = id;
     }
 
+    public void ResetDragObject()
+    {
+        // Ensure we have our references before trying to use them
+        Initialize();
+
+        this.enabled = true;
+        if (image != null) image.enabled = true;
+        if (rectTransform != null) rectTransform.anchoredPosition = originalPosition;
+
+        checkforCollision = false;
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
+        Initialize(); // Safety check
         rectTransform.SetAsLastSibling();
         checkforCollision = false;
     }
@@ -39,13 +69,22 @@ public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
     public void OnTriggerStay2D(Collider2D collision)
     {
-        if (checkforCollision) 
+        if (checkforCollision)
         {
             if (collision.CompareTag("ColorZone"))
             {
-                manager.CheckRightColorMatch(answerId,collision.GetComponent<ColorZone>().answerId);
-                if (answerId == collision.GetComponent<ColorZone>().answerId) Destroy(this);
-                checkforCollision = false;
+                ColorZone zone = collision.GetComponent<ColorZone>();
+                if (zone != null)
+                {
+                    manager.CheckRightColorMatch(answerId, zone.answerId);
+
+                    if (answerId == zone.answerId)
+                    {
+                        if (image != null) image.enabled = false;
+                        this.enabled = false;
+                    }
+                    checkforCollision = false;
+                }
             }
         }
     }
